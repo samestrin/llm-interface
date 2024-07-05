827105fc9d7e82da21a57b00107e534b6eb8af22
Sam Estrin <sam.estrin@gmail.com>
2024-07-04 18:25:54 -0700

    test: updating tests to prevent api errors

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
+  maxWorkers: 5,
   testTimeout: 30000, // Set global timeout to 30 seconds
   snapshotSerializers: ['<rootDir>/src/utils/jestSerializer.js'],
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,11 @@
 // jest.setup.js
 require = require('esm')(module /*, options*/);
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+beforeEach(async () => {
+  // Add a delay before each test
+  await delay(5000); // Adjust the delay time as needed
+});
